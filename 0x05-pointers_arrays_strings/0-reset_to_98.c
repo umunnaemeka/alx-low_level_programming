ef5562ae9c86afef2a44cdbe5029ff4524f0cef2
umunnaemeka <umunnaemeka@gmail.com>
2022-09-19 05:11:52 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,8 +7,8 @@
  * @n: an integer pointer that stores the address argument
  * Retun: returns nothing
  */
-  void reset_to_98 (int  *n)
+  void reset_to_98(int  *n)
   
   {
-    *n = 98;
-    }
+    *n=98;
+  }
