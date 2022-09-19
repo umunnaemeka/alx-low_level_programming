95305c2c11a53ef689f31d655c4b24f557be98a0
umunnaemeka <umunnaemeka@gmail.com>
2022-09-19 05:19:52 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes an integet pointer and 
- * updates the value it points to , to 98
- * @n: an integer pointer that stores the address argument
- * Retun: returns nothing
- */
-
-  void reset_to_98(int *n)
-  
-  {
-    *n = 98;
-  }
+* reset_to_98 - takes an integer pointer and
+* updates the value it points to to 98
+* @n: an integer pointer that stores the address argument
+* Return: returns nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
