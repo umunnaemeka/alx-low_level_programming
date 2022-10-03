df25472ec903f76630a6173e011f5d405f2b9f54
umunnaemeka <umunnaemeka@gmail.com>
2022-10-03 04:15:56 -0700

    Function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,67 +1,139 @@
-include "main.h"
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+int count_words(char *str);
+int *count_char(char *strlen, int c);
+char **split_string(char **arrystn, char *str);
+
 
 /**
- * ch_free_grid - frees a 2 dimensional array.
- * @grid: multidimensional array of char.
- * @height: height of the array.
- *
- * Return: no return
- */
-void ch_free_grid(char **grid, unsigned int height)
-{
-	if (grid != NULL && height != 0)
-	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
-		free(grid);
-	}
-}
-
-/**
- * strtow - splits a string into words.
- * @str: string.
- *
- * Return: pointer of an array of integers
- */
+* strtow - a function that splits a string into words
+* @str: a ponter to the string to be operated on
+* Return: returns a pointer to a pointer to a string
+*/
 char **strtow(char *str)
 {
-	char **aout;
-	unsigned int c, height, i, j, a1;
+	int i;
+	int c = 0;
+	int *strlen;
+	char **arrystn;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL)
 		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-	aout = malloc((height + 1) * sizeof(char *));
-	if (aout == NULL || height == 0)
+
+	for (i = 0; str[i] != '\0'; i++)
+		c++;
+	if (c == 0)
+		return (NULL);
+
+	c = count_words(str);
+	strlen = count_char(str, c);
+
+	arrystn = (char **) malloc(c * sizeof(char *));
+	if (arrystn == NULL)
+		return (NULL);
+
+	for (i = 0; i < c; i++)
 	{
-		free(aout);
-		return (NULL);
+		arrystn[i] = malloc((strlen[i] + 1) * sizeof(char));
+		if (arrystn[i] == NULL)
+			return (NULL);
 	}
-	for (i = a1 = 0; i < height; i++)
+
+	arrystn = split_string(arrystn, str);
+	return (arrystn);
+}
+
+/**
+* count_words - counts the number of words in str, assuming every word is
+* seperated by spaces
+* @str: a pointer to the string to be counted
+* Return: returns an int
+*/
+int count_words(char *str)
+{
+	int i;
+	int c = 0;
+
+	for (i = 0; str[i] != 0; i++)
 	{
-		for (c = a1; str[c] != '\0'; c++)
+		if (str[i] != ' ' && str[i - 1] == ' ')
 		{
-			if (str[c] == ' ')
-				a1++;
-			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			c++;
+		}
+	}
+	return (c);
+}
+
+/**
+* count_char - counts the number of characters in each word and stores it
+* in an array of integers
+* @str: the string with the words to be counted
+* @c: the number of words in str
+* Return: returns a pointer to the array of integers containing
+* the size of each word
+*/
+int *count_char(char *str, int c)
+{
+	int i = 0, j = 0, s = 0, k = 0;
+
+	int *strlen = malloc(c * sizeof(int));
+
+	if (strlen == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s = 0;
+		if (str[i] != ' ' && str[i - 1] == ' ')
+		{
+			for (j = i; str[j] != '\0'; j++)
 			{
-				aout[i] = malloc((c - a1 + 2) * sizeof(char));
-				if (aout[i] == NULL)
+				if (str[j]  == ' ')
 				{
-					ch_free_grid(aout, i);
-					return (NULL);
+					strlen[k] = s;
+					k++;
+					break;
 				}
-				break;
+				s++;
 			}
 		}
-		for (j = 0; a1 <= c; a1++, j++)
-			aout[i][j] = str[a1];
-		aout[i][j] = '\0';
 	}
-	aout[i] = NULL;
-	return (aout);
+	return (strlen);
 }
+
+/**
+* split_string - puts each word from the original string str into a new
+* an array of strings arrystn, each string in the array is null-terminated
+* @arrystn: the array of strings
+* @str: the original string
+* Return: returns arrystn as a pointer to a pointer
+*/
+char **split_string(char **arrystn, char *str)
+{
+	int i, j, k, s;
+
+	k = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		s = 0;
+		if (str[i] != ' ' && str[i - 1] == ' ')
+		{
+			for (j = i; str[j] != '\0'; j++)
+			{
+				if (str[j]  == ' ')
+				{
+					arrystn[k][j] = '\0';
+					break;
+				}
+				arrystn[k][s] = str[j];
+				s++;
+			}
+			k++;
+		}
+	}
+	arrystn[k] = NULL;
+	return (arrystn);
+}
+
