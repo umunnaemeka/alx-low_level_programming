2943869a73ad309e126fe86e1473c44209d8906f
umunnaemeka <umunnaemeka@gmail.com>
2022-10-25 06:11:14 -0700

    Function that adds a new node.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
-  * print_list - prints all the elements of a list_t list.
-  * @h: singly linked list.
-  * Return: number of elements in the list.
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
 
 size_t print_list(const list_t *h)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
