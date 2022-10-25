a5020ea4d0db5830b554b8ded8d9828ff7416235
umunnaemeka <umunnaemeka@gmail.com>
2022-10-25 06:47:11 -0700

    Function that prints all the elements.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 
 size_t print_list(const list_t *h)
 {
-		size_t nelem;
+	size_t nelem;
 
-		nelem = 0;
-		while (h != NULL)
-		{
-				if (h->str == NULL)
-				printf("[%d] %s\n", 0, "(nil)");
-			else
-				printf("[%d] %s\n", h->len, h->str);
-						h = h->next;
-						nelem++;
-		}
-					return (nelem);
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
